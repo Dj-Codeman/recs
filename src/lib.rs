de4e419925cb8c0f0d9a52df862c5dbd6fc41ec9
Darrion Whitfield <dwhitfield@ramfield.net>
2023-08-01 23:20:05 -0500

    adding ping

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,32 @@
-#[path = "system/system.rs"] mod system;
-#[path = "system/encrypt.rs"] pub mod encrypt;
-#[path = "system/secrets.rs"] mod secret;
-#[path = "system/config.rs"] mod config;
-#[path = "auth.rs"] mod auth;
-#[path = "enviornment.rs"] mod local_env;
+#[path = "auth.rs"]
+mod auth;
+#[path = "system/config.rs"]
+mod config;
+#[path = "system/encrypt.rs"]
+pub mod encrypt;
+#[path = "enviornment.rs"]
+mod local_env;
+#[path = "system/secrets.rs"]
+mod secret;
+#[path = "system/system.rs"]
+pub mod system; // make this its own library
 
-use std::{process::exit, fs::{File, OpenOptions}, io::{Read, Write}, };
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+    process::exit,
+};
 
 use crate::{
     auth::{fetch_chunk, index_system_array, ChunkMap},
+    config::{
+        ARRAY_LEN, CHUNK_SIZE, DEBUG, PUBLIC_MAP_DIRECTORY, SECRET_MAP_DIRECTORY,
+        SYSTEM_ARRAY_LOCATION,
+    },
     encrypt::create_hash,
-    system::{append_log, exist, VERSION, unexist}, 
-    secret::{write, read, forget},
     local_env::set_system,
-    config::{SYSTEM_ARRAY_LOCATION, ARRAY_LEN, CHUNK_SIZE, PUBLIC_MAP_DIRECTORY, DEBUG},
+    secret::{forget, read, write},
+    system::{append_log, exist, unexist, VERSION},
 };
 
 // !? Allow this as a toggle flag later
@@ -21,7 +34,6 @@ fn check_debug() {
     use std::env;
     env::set_var("RUST_BACKTRACE", "1");
 }
-
 
 pub fn initialize() {
     if DEBUG == true {
@@ -45,10 +57,9 @@ pub fn initialize() {
         // * to avoid heart ache if we find an existing system array we just re index it instead of deleting everything
         index_system_array();
     }
-
 }
 
-// Normal actions 
+// Normal actions
 pub fn insert(filename: String, owner: String, name: String) -> Option<bool> {
     if !write(filename, owner, name) {
         exit(1)
@@ -70,15 +81,30 @@ pub fn remove(owner: String, name: String) -> Option<bool> {
     return Some(true);
 }
 
+pub fn ping(owner: String, name: String) -> bool {
+    let mut secret_map_path: String = String::new();
+    secret_map_path.push_str(SECRET_MAP_DIRECTORY);
+    secret_map_path.push_str("/");
+    secret_map_path.push_str(&owner);
+    secret_map_path.push_str("-");
+    secret_map_path.push_str(&name);
+    secret_map_path.push_str(".json");
+
+    if !exist(&secret_map_path) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 // Debugging and tooling
 
 pub fn check_map(map_num: u32) -> bool {
     let _ = fetch_chunk(map_num); // using fetch chunk to validate the map data
-    return true
+    return true;
 }
 
 pub fn update_map(map_num: u32) -> bool {
-
     // ? Getting the current map data
     let mut map_path: String = String::new();
     map_path.push_str(PUBLIC_MAP_DIRECTORY);
@@ -89,11 +115,11 @@ pub fn update_map(map_num: u32) -> bool {
     // ? Reading the map
     let mut map_file = File::open(&map_path).expect("File could not be opened");
     let mut map_data: String = String::new();
-    
+
     map_file
         .read_to_string(&mut map_data)
         .expect("Could not read the map file !");
-    
+
     // ? unpacking to the chunk map struct
     let pretty_map_data: ChunkMap = serde_json::from_str(&map_data).unwrap();
 
@@ -110,23 +136,23 @@ pub fn update_map(map_num: u32) -> bool {
         chunk_end: pretty_map_data.chunk_end,
     };
 
-    // write the new map file 
+    // write the new map file
     unexist(&map_path);
     let updated_map = serde_json::to_string_pretty(&new_map).unwrap();
 
     let mut map_file = OpenOptions::new()
-    .create_new(true)
-    .write(true)
-    .append(true)
-    .open(map_path)
-    .expect("File could not written to");
+        .create_new(true)
+        .write(true)
+        .append(true)
+        .open(map_path)
+        .expect("File could not written to");
 
     if let Err(_e) = writeln!(map_file, "{}", updated_map) {
         eprintln!("An error occoured");
         append_log("Could save map data to file");
     };
 
-    return true
+    return true;
 }
 
 pub fn index_array() -> Option<bool> {
