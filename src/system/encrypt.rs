425b53835761dca24afb2cd160884551809c5213
Darrion Whitfield <dwhitfield@ramfield.net>
2024-02-03 02:10:09 -0600

    getting blocking ugly

--- src/system/encrypt.rs
+++ src/system/encrypt.rs
@@ -58,12 +58,11 @@ pub fn encrypt(
     })?;
 
     let pad_len = data.len();
-    // let mut buffer: Vec<u8> = if pad_len > buffer_size {
-    //     vec![0; buffer_size * 2]
-    // } else {
-    //     vec![0; buffer_size]
-    // };
-    let mut buffer: Vec<u8> = vec![0; pad_len];
+    let mut buffer: Vec<u8> = if pad_len > buffer_size {
+        vec![0; buffer_size * 2]
+    } else {
+        vec![0; buffer_size]
+    };
 
     buffer[..pad_len].copy_from_slice(&data);
 
