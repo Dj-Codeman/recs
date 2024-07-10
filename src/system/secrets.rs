5b82e5308d956ae4e0588197954fb361d2728353
Darrion Whitfield <dwhitfield@ramfield.net>
2024-07-09 22:09:07 -0500

    read_raw()

--- src/system/secrets.rs
+++ src/system/secrets.rs
@@ -541,9 +541,9 @@ pub fn read_raw(
             };
 
             // take the first splinting chunk into signature and cipher data
-            let encoded_signature: &str = truncate(&secret_buffer, 64);
+            let encoded_signature: &str = truncate(&secret_buffer, 62);
             // ! When this inevitably fails, Remember the paddingcount() changes the sig length.
-            let cipher_buffer: &str = &secret_buffer[64..]; // * this is the encrypted hex encoded bytes
+            let cipher_buffer: &str = &secret_buffer[63..]; // * this is the encrypted hex encoded bytes
 
             // * decrypting the chunk
             let mut decrypted_data: Vec<u8> =
@@ -811,7 +811,6 @@ pub fn read(
                     // take the first spliiting chunk into signature and cipher data
                     let encoded_signature: &str = truncate(&secret_buffer, 62); // 61 + how ever big the chunk count is
                     let cipher_buffer: &str = &secret_buffer[63..];
-                    panic!("{}", cipher_buffer);
 
                     // * decrypting the chunk
                     let mut decrypted_data: Vec<u8> =
